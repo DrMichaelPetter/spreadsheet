72e4c18db4cdc45dece321e37972ed4059399079
Dr. Michael Petter <petter@in.tum.de>
2023-11-20 10:38:33 +0100

    initial release

---
+++ java/schule/Expr.java
@@ -0,0 +1,325 @@
+package schule;
+
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public abstract class Expr {
+    public int eval(Spreadsheet sheet)throws Exception {
+        return eval(sheet,new HashSet<Expr>());
+    }
+    /**
+     * evaluate this (sub)expression, considering evalset as already evaluated cells (i.o. to detect cyclic evals)
+     * @param sheet
+     * @param evalset
+     * @return
+     * @throws Exception
+     */
+    public abstract int eval(Spreadsheet sheet,Set<Expr> evalset) throws Exception;    
+    /**
+     * puts a String representation of this expression as a formula on the stream
+     * @param stream
+     */
+    public abstract void replicateTo(PrintStream stream);
+    public static class Const extends Expr{
+        int value;
+        @Override
+        public int eval(Spreadsheet sheet,Set<Expr> evalset) {
+            return value;
+        }
+        public Const(int val){
+            value=val;
+        }
+        @Override
+        public void replicateTo(PrintStream stream) {
+            stream.print(value);
+        }
+        @Override
+        public String toString() {
+            return ""+value;
+        }
+    }
+    public static class Ref extends Expr {
+        int col,row;
+        @Override
+        public int eval(Spreadsheet sheet,Set<Expr> evalset) throws Exception{
+            return sheet.eval(row,col,evalset);
+        }
+        public Ref(String ref){
+            this((ref.charAt(0)) - 'A',Integer.parseInt(ref.substring(1))-1);
+        }
+        private Ref(int col,int row) { this.col=col;this.row=row;}
+        @Override
+        public void replicateTo(PrintStream stream) {
+            stream.print(""+((char)(col+'A'))+(row+1));
+        }
+        @Override
+        public String toString() {
+            return ""+(char)('A'+col)+(row+1);
+        }
+    }
+    public static class BinEx extends Expr {
+        Expr l,r;
+        String op;
+        @Override
+        public int eval(Spreadsheet sheet,Set<Expr> evalset) throws Exception {
+            switch (op){
+                case "+": return l.eval(sheet,evalset) + r.eval(sheet,evalset);
+                case "*": return l.eval(sheet,evalset) * r.eval(sheet,evalset);
+                case "/": return l.eval(sheet,evalset) / r.eval(sheet,evalset);
+                case "-": return l.eval(sheet,evalset) - r.eval(sheet,evalset);
+                default: return 0;
+            }
+        }
+        public BinEx(Expr l,String op,Expr r) { this.l=l;this.op=op;this.r=r;}
+        @Override
+        public void replicateTo(PrintStream stream) {
+            stream.print("(");
+            l.replicateTo(stream);
+            stream.print(op);
+            r.replicateTo(stream);
+            stream.print(")");
+        }
+        @Override
+        public String toString() {
+            return "("+l+" "+op+" "+r+")";
+        }
+    }
+    public static class CallEx extends Expr {
+        List<Expr> params;
+        String name;
+        public CallEx(String name, List<Expr> params){
+            this.name=name;
+            this.params=params;
+        }
+        @Override
+        public int eval(Spreadsheet sheet,Set<Expr> evalset) throws Exception {
+            List<Integer> l = new LinkedList<>();
+            for (var e:params){
+                l.add(e.eval(sheet, evalset));
+            }
+            return sheet.lookupFunction(name).apply(l);
+        }
+        @Override
+        public void replicateTo(PrintStream stream) {
+            stream.print(name);
+            stream.print("(");
+            if (params.size()>0){
+                params.get(0).replicateTo(stream);
+                if (params.size()>1)
+                    params.subList(1,params.size()) .forEach(p-> { stream.print(",");p.replicateTo(stream);} );
+            }
+            stream.print(")");
+        }
+        @Override
+        public String toString() {
+            var p=params.stream().map(x->x.toString()).collect(Collectors.joining(","));
+            return "("+name+"("+p+"))";
+        }
+    }
+    public static Expr parse(String expression)throws Parser.Fail {
+        var tokenstream = scan(expression);
+        // we sort out all the pesky whitespaces
+        tokenstream = tokenstream.stream()
+            .filter(c->c.type!=TokenType.WHITESPACE)
+            .collect(Collectors.toList());
+        // hand over the token stream to the parser
+        var p = new Parser(tokenstream);
+        // return a java object representation of the syntactic structure of the expression
+        return p.expr();
+    }
+    /**
+     * Iteratively decapitate the input string, matching the current start of the string with the patterns
+     * for the respective tokens of our spreadsheet language. 
+     * @param expression
+     * @return the input expression converted to a List of classified tokens
+     */
+    public static List<Token> scan(String expression){
+        var tokenstream = new LinkedList<Token>();
+        // repeat process as long as there is input to be scanned
+        scanning: while (expression.length()>0){
+            // iterate through all token types
+            tokens: for (var tok:TokenType.values()) {
+                // build a matcher for the current pattern
+                var patty = tok.getPattern();
+                var matchy = patty.matcher(expression);
+                if (matchy.find()){ // input matched the pattern for this token type
+                    var lex=matchy.group();
+                    tokenstream.add(new Token(tok,lex));
+                    // split off the found token from the input and continue scanning
+                    expression=expression.substring(lex.length());
+                    continue scanning;
+                }
+            }
+            throw new RuntimeException("nasty input found!");
+        }
+        tokenstream.add(new Token(TokenType.EOF,""));
+        return tokenstream;
+    }
+
+    /**
+     * specifies TokenTypes for the spreadlanguage, together with their lexicographic pattern
+     */
+    public static enum TokenType {
+        MULOP("(\\*|/)"),
+        ADDOP("(\\+|-)"),
+        COMMA(","),
+        LBRACK("\\("),
+        RBRACK("\\)"),
+        RANGE(":"),
+        REF("[A-Pa-p]1?\\d"),
+        INTCONST("\\d+"),
+        NAME("\\w+"),
+        WHITESPACE("\\s"),
+        CATCHALL(".*"),
+        EOF("");
+        private Pattern pattern;
+        TokenType(String pattern){
+            this.pattern=Pattern.compile("\\A"+pattern);
+        }
+        /**
+         * @return regex pattern that matches tokens of this type
+         */
+        public Pattern getPattern(){
+            return pattern;
+        }
+    }
+    /**
+     * represents a classified input substring
+     * - is generated by the scanner
+     * - is consumed by the parser
+     */
+    public record Token(TokenType type,String input) {};
+
+    /**
+     * Recursive Descent Parser for the spreadlanguage
+     * 
+     */
+    public static class Parser {
+        public class Fail extends Exception {
+            public Fail(String message){
+                super(message);
+            }
+        }
+        private List<Token> terminals;
+        /**
+         * consumes (i.e. removes) whatever token is the current head of the input
+         * @return
+         */
+        public Token consume(){
+            return terminals.remove(0);
+        }
+        /**
+         * consumes (i.e. removes) token from the rest of the input stream
+         * @param t the expected token to be found at the head of the input
+         * @return
+         * @throws Fail
+         */
+        public Token consume(TokenType t) throws Fail {
+            if (peek()!=t) throw new Fail("expected "+t+" , but found "+consume());
+            return consume();
+        }
+        /**
+         * check what the head of the remaining input looks like
+         * @return head of the remaining input
+         */
+        public TokenType peek(){
+            return terminals.get(0).type();
+        }
+        /**
+         * Handover of the Tokenlist to the parser engine
+         * @param terminals
+         */
+        public Parser(List<Token> terminals){
+            this.terminals=terminals;
+        }
+        /**
+         * Entry function into parsing expressions
+         * @return
+         * @throws Fail
+         */
+        public Expr expr() throws Fail {
+            return e();
+        }
+// C -> name ( C , ... )
+        private Expr c() throws Fail {
+            var name=NAME();
+            List<Expr> params=new LinkedList<Expr>();
+            consume(TokenType.LBRACK);
+            while (peek()!=TokenType.RBRACK) { 
+                params.add(e());
+                if (peek()!=TokenType.COMMA) {
+                    if (peek()!=TokenType.RBRACK) throw new Fail("expected , or ) but found "+consume());
+                }
+                else consume(TokenType.COMMA);
+            }
+            consume(TokenType.RBRACK);
+            var e = new CallEx(name,params);
+            return e;
+        }
+// E  -> T E'
+// E' -> + T E' | -TE' |epsilon
+        /**
+         * D&C handle the parsing of an expression
+         * @return
+         * @throws Fail
+         */
+        private Expr e() throws Fail {
+            var e = t();
+            while ((peek())==TokenType.ADDOP){
+                var t = consume(TokenType.ADDOP);
+                var e2 = t();
+                e = new BinEx(e,t.input(),e2);
+            }
+            return e;
+        }
+// T  -> F T'
+// T' -> * F T' | /FT' |epsilon
+        /**
+         * D&C handling of the parsing of a multiplicative term
+         * @return
+         * @throws Fail
+         */
+        private Expr t() throws Fail{
+            var e = f();
+            while ((peek())==TokenType.MULOP){
+                var t = consume(TokenType.MULOP);
+                var e2 = f();
+                e = new BinEx(e,t.input(),e2);
+            }
+            return e;
+        }
+// F  -> (E) | int | ref | C
+        /**
+         * D&C handling of the parsing of a factor
+         * @return
+         * @throws Fail
+         */
+        private Expr f() throws Fail{
+            switch (peek()){
+                case INTCONST: return INTCONST();
+                case REF: return REF();
+                case NAME: return c();
+                case LBRACK:
+                    consume(TokenType.LBRACK);
+                    var e = expr();
+                    consume(TokenType.RBRACK);
+                    return e;
+                default: throw new Fail("expected an INTCONST, REF  or ( but found "+consume());
+            }
+        }
+        private Expr INTCONST() throws Fail {
+            return new Const(Integer.parseInt(consume(TokenType.INTCONST).input()));
+        }
+        private Expr REF() throws Fail {
+            return new Ref(consume(TokenType.REF).input());
+        }
+        private String NAME() throws Fail {
+            return consume(TokenType.NAME).input();
+        }
+    }
+}
---
+++ java/schule/Spreadsheet.java
@@ -0,0 +1,268 @@
+package schule;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.gui2.dialogs.TextInputDialog;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+public class Spreadsheet {
+    public static final int DIGITS=3;
+    public static final int ROWS=16;
+    public static final int COLS=ROWS;
+    private Optional<Expr>[][] formulae = new Optional[ROWS][COLS];
+    public Optional<Integer>[][] values = new Optional[ROWS][COLS];
+    /**
+     * Gives the optionally empty raw Formula for [row/col] back, e.g. A1*5+B3
+     * @param row
+     * @param col
+     * @return
+     */
+    public Optional<Expr> getFormula(int row, int col){
+        return formulae[row][col];
+    }
+    public void setFormula(int row,int col,Optional<Expr> form) { 
+        formulae[row][col]=form; 
+    }
+    private int cursorcol=0,cursorrow=0;
+    private Map<String,Function<List<Integer>,Integer>> funcRegistry = new HashMap<>();
+    public Function<List<Integer>,Integer> lookupFunction(String name){
+        return funcRegistry.get(name.toUpperCase());
+    }
+    public Spreadsheet(){
+        for (int i=0;i<ROWS;i++) 
+            for (int j=0;j<COLS;j++)
+                setFormula(i, j, Optional.empty());
+        funcRegistry.put("MAX",x -> Math.max(x.get(0),x.get(1)));
+    }
+    /**
+     * To start evaluation anew, we need to purge all old cached values from values[][]
+     */
+    private void purgeValues(){
+        values = new Optional[ROWS][COLS];
+    }
+    /**
+     *  print out the frame for a Cell
+     */
+    private static void drawCellToConsole(TextGraphics textGraphics, int row ,int col , String txt) {
+        col++;
+        int x0 = col * (3+DIGITS);
+        int y0 = row * 2;
+        int x1 = x0 + (3+DIGITS);
+        int y1 = y0 + 2;
+        textGraphics.drawRectangle(new TerminalPosition(x0, y0), new TerminalSize(4+DIGITS, 3), (char)0x2500); //-
+        textGraphics.drawLine(x0,y0,x0,y1,(char)0x2502); // |
+        textGraphics.drawLine(x1,y0,x1,y1,(char)0x2502); // |
+        textGraphics.setCharacter(x0,y0,(char)0x253C);   // +
+        textGraphics.setCharacter(x0,y1,(char)0x253C);   // +
+        textGraphics.setCharacter(x1,y0,(char)0x253C);   // +
+        textGraphics.setCharacter(x1,y1,(char)0x253C);   // +
+        textGraphics.putString(x0+1,y0+1," "+txt+" ");
+    }
+
+    /**
+     *  print out the actual content of the cell, and delegate to drawCellToConsole for the frame 
+     */
+    private void drawDataCellToConsole(TextGraphics textgraphics, int row,int col){
+        var str = "   ";
+        if (!getFormula(row,col).isEmpty())  {
+            try {
+                str=String.format("%"+DIGITS+"d",eval(row,col));
+            }catch(Exception e){
+                str="### "+e.getMessage();
+            }
+        }
+        drawCellToConsole(textgraphics, row+1,col, str);
+    }
+    /**
+     *  prints the whole array of cells to the console, and evens out some borderframes
+     */
+    public String printToConsole(Screen screen, TextGraphics textGraphics) throws IOException, InterruptedException {
+        screen.clear();
+        IntStream.range(0,COLS).forEachOrdered(cols ->
+            drawCellToConsole(textGraphics, 0, cols, " "+(char)(cols+'A'))
+            );
+        for (int cols = 0; cols < COLS;cols++)
+            for (int rows=0; rows< ROWS; rows++)
+                drawDataCellToConsole(textGraphics, rows,cols);
+        for (int j=0; j< COLS+1; j++) { // Finalize Border lines
+            textGraphics.setCharacter(6,j*2,(char)0x251C);              // ├
+            textGraphics.setCharacter(6*(COLS+1),j*2,(char)0x2524);     // ┤
+            textGraphics.setCharacter(6*(j+1),0,(char)0x252C);          // ┬
+            textGraphics.setCharacter(6*(j+1),(ROWS+1)*2,(char)0x2534); // ┴
+            textGraphics.putString(0,1+(j)*2," "+j);
+        }
+        // red frame around the active cell:
+        textGraphics.setForegroundColor(TextColor.ANSI.RED);
+        drawDataCellToConsole(textGraphics,cursorrow,cursorcol);
+        textGraphics.setForegroundColor(TextColor.ANSI.DEFAULT);
+        // little prompt at the bottom line:
+        var cell = "Current Cell "+((char)(cursorcol+'A'))+(cursorrow+1)+": ";
+        var raw = getFormula(cursorrow,cursorcol);
+        var bo = new ByteArrayOutputStream();
+        if (raw.isPresent()) raw.get().replicateTo(new PrintStream(bo));
+        cell+="="+bo.toString();
+        textGraphics.putString(1,2*(ROWS+2),cell);
+        // cursor to indicate readyness for new input
+        screen.setCursorPosition(new TerminalPosition(1+cell.length(), 2*(ROWS+2)));
+        return bo.toString();
+    }
+
+    public void writeToCSV(String filename) throws IOException {
+        var pw = new PrintWriter(new File(filename));
+        for (var r:formulae){
+            pw.println(Arrays.stream(r)
+                .map(x->(x.isPresent())?("="+x.get().toString()):"")
+                .collect(Collectors.joining(";")));
+        }
+        pw.close();
+    }
+    /**
+     * evaluate the expression at position Row / Col, caching its value in value[][] as sideeffect; this is the entry point into an evaluation, starting without any already touched references
+     * @param row
+     * @param col
+     * @return
+     * @throws Exception
+     */
+    public int eval(int row, int col) throws Exception {
+        return eval(row,col,new HashSet<Expr>());
+    }
+    /**
+     * evaluate the expression at position Row / Col, caching its value in value[][] as sideeffect; this version explicitely starts with an initial set of already touched references
+     * @param row
+     * @param col
+     * @param refset set of so far evaluated references i.o. to detect cycles
+     * @return final integer value
+     * @throws Exception
+     */
+    public int eval(int row, int col,Set<Expr> refset) throws Exception {
+        if (values[row][col]==null) {
+            if (!getFormula(row,col).isPresent())
+                values[row][col]=Optional.empty();
+            else {
+                var e = getFormula(row,col).get();
+                var hs = new HashSet<Expr>(refset);
+                if (refset.contains(e)) {
+                    ByteArrayOutputStream bo = new ByteArrayOutputStream();
+                    e.replicateTo(new PrintStream(bo));
+                    var str = refset.stream()
+                        .map(x->x.toString())
+                        .collect(Collectors.joining(","));
+                    throw new Exception("Circular evaluation during evaluation of "+bo.toString()+" : "+str);
+                }
+                hs.add(e);
+                // caches evaluated formula[x][y] in value[x][y]
+                values[row][col]=Optional.of(e.eval(this, hs));
+            }
+        }
+        return values[row][col].get();
+    }
+    /**
+     * Takes a string representation of a spreadsheet formula or value (e.g. =5*A1+B5 or 42) and returns a literal formula object
+     * @param cell
+     * @return
+     */
+    public static Optional<Expr> parseCell(String cell){
+        if (cell.isEmpty()) return Optional.empty();
+        else {
+            if (cell.startsWith("=")) {
+                try {
+                    return Optional.of(Expr.parse(cell.substring(1)));
+                }catch(Expr.Parser.Fail f){
+                    return Optional.empty();
+                }
+            }
+            else {
+                return Optional.of(new Expr.Const(Integer.parseInt(cell)));
+            }
+        }
+    }
+    public static Spreadsheet parseCSV(String filename) throws FileNotFoundException,IOException,NumberFormatException {
+        var result = new Spreadsheet();
+        var buffy = new BufferedReader(new FileReader(filename));
+        var row =0;
+        while (buffy.ready() && row < result.ROWS) {
+            var scanner = new Scanner(buffy.readLine()).useDelimiter(";");
+            var col = 0;
+            while(scanner.hasNext()){
+                result.setFormula(row,col++,parseCell(scanner.next()));        
+            }
+            row++;
+            scanner.close();
+        }
+        buffy.close();
+        return result;
+    }
+    public static void main(String[] args) throws Exception{
+        var e = parseCSV(args[0]);
+        var terminal = new DefaultTerminalFactory().createTerminal();
+        var screen = new TerminalScreen(terminal);
+        screen.startScreen();
+        var textGraphics = screen.newTextGraphics();
+        KeyStroke key;
+        final WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
+        do {
+            var content = e.printToConsole(screen,textGraphics);
+            screen.refresh();
+            key=screen.readInput();
+            switch (key.getKeyType()) {
+                case ArrowDown:  e.cursorrow=(16+e.cursorrow+1)%16; break;
+                case ArrowUp:    e.cursorrow=(16+e.cursorrow-1)%16; break;
+                case ArrowLeft:  e.cursorcol=(16+e.cursorcol-1)%16; break;
+                case ArrowRight: e.cursorcol=(16+e.cursorcol+1)%16; break;
+                case Enter: 
+                      String input = TextInputDialog.showDialog(textGUI,"Content for Cell","edit the content of Cell ","="+content);
+                      e.setFormula(e.cursorrow,e.cursorcol,parseCell(input));
+                      e.purgeValues();                     
+                      break;
+                default:
+            }
+        }
+        while (key.getKeyType()!=KeyType.Escape);
+        screen.stopScreen();
+        screen.close();
+        e.writeToCSV("out.csv");
+        System.out.println(e);
+   }
+   @Override
+   public String toString() {
+        var sb  = new StringBuffer();
+        for (var line : values) {
+            if (line==null) sb.append("\n");
+            else {
+                var zeile = Arrays.stream(line)
+                    .map(c -> (c!=null && c.isPresent())?String.format("%"+3+"d",c.get()):"   ")
+                    .collect(Collectors.joining(" - "));
+                sb.append(zeile+"\n");
+            }
+        }
+        return sb.toString();
+   }
+}  
